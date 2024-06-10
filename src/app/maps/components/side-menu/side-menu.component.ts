07723b696cf8f8e10e847d1351fc04f48cc9323a
Mariano <macacozzz@hotmail.com>
2024-06-10 11:12:11 -0300

    floating side menu

--- src/app/maps/components/side-menu/side-menu.component.ts
+++ src/app/maps/components/side-menu/side-menu.component.ts
@@ -1,18 +1,39 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+interface MenuItem {
+  name: string;
+  route: string;
+}
 
 @Component({
-  selector: 'app-side-menu',
+  selector: 'maps-side-menu',
   standalone: true,
   imports: [
     CommonModule,
+    RouterModule
   ],
   templateUrl: './side-menu.component.html',
   styles: `
     :host {
       display: block;
     }
+
+  li {
+    cursor: pointer;
+    transition: 0.2s all;
+  }
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SideMenuComponent { }
+export class SideMenuComponent {
+
+  public menuItems: MenuItem[] =[
+    {route: '/maps/fullscreen', name: 'FullScreen'},
+    {route: '/maps/zoom-range', name: 'ZoomRange'},
+    {route: '/maps/markers', name: 'Markers'},
+    {route: '/maps/properties', name: 'Houses'},
+]
+
+ }
--- src/app/maps/layout/maps-layout/maps-layout.component.ts
+++ src/app/maps/layout/maps-layout/maps-layout.component.ts
@@ -1,19 +1,30 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { SideMenuComponent } from "../../components/side-menu/side-menu.component";
 
 @Component({
-  standalone: true,
-  imports: [
-    CommonModule,
-    RouterModule
-  ],
-  templateUrl: './maps-layout.component.html',
-  styles: `
+    standalone: true,
+    templateUrl: './maps-layout.component.html',
+    styles: `
     :host {
       display: block;
     }
+
+    maps-side-menu {
+      position: fixed;
+      top: 20px;
+      left: 20px;
+      z-index: 999;
+      width: 180px;
+
+    }
   `,
-  changeDetection: ChangeDetectionStrategy.OnPush,
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    imports: [
+        CommonModule,
+        RouterModule,
+        SideMenuComponent
+    ]
 })
 export class MapsLayoutComponent { }
