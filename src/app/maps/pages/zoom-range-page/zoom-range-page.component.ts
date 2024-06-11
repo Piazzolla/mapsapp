6c8f087e0f7a5df8f7b5584054c7247637896711
Mariano <macacozzz@hotmail.com>
2024-06-11 11:02:12 -0300

    zoom range page husk

--- src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
+++ src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
@@ -1,5 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { AfterViewInit, ChangeDetectionStrategy, Component, ElementRef, ViewChild } from '@angular/core';
+import mapboxgl from 'mapbox-gl';
+import { environment } from '../../../../environments/environment';
 
 @Component({
   selector: 'app-zoom-range-page',
@@ -12,7 +14,55 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
     :host {
       display: block;
     }
-  `,
+
+    #map {
+      width: 100vw;
+      height: 100vh;
+
+    }
+
+    .floating-range {
+      position: fixed;
+      bottom: 20px;
+      left: 20px;
+      z-index: 999;
+      width: 500px;
+      background-color: white;
+      border-radius: 10px;
+      box-shadow: 0px 5px 10px rgba(0,0,0, 0.1);
+      }
+
+      .floating-content {
+        display: flex;
+        align-items: center;
+      }
+
+      button {
+        width: 40px;
+      }
+
+
+    `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ZoomRangePageComponent { }
+export class ZoomRangePageComponent implements AfterViewInit {
+
+  @ViewChild('map')
+  public divMap?: ElementRef;
+
+  ngAfterViewInit(): void {
+
+    if (!this.divMap) throw 'HTML element not found';
+
+    const map = new mapboxgl.Map({
+      accessToken: environment.mapbox_key,
+      container: this.divMap?.nativeElement, // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: [-74.5, 40], // starting position [lng, lat]
+      zoom: 9, // starting zoom
+    });
+  }
+
+}
+
+
