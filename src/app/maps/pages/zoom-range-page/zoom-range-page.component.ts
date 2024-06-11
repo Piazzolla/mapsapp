ed6db5c1e7ea30242e659c6bb96a5db69684c4d2
Mariano <macacozzz@hotmail.com>
2024-06-11 16:47:46 -0300

    zoom bar feature

--- src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
+++ src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { AfterViewInit, ChangeDetectionStrategy, Component, ElementRef, ViewChild } from '@angular/core';
+import { AfterViewInit, ChangeDetectionStrategy, ChangeDetectorRef, Component, ElementRef, ViewChild } from '@angular/core';
 import mapboxgl from 'mapbox-gl';
 import { environment } from '../../../../environments/environment';
 
@@ -50,17 +50,52 @@ export class ZoomRangePageComponent implements AfterViewInit {
   @ViewChild('map')
   public divMap?: ElementRef;
 
+  public zoom: number = 10;
+  public map?: mapboxgl.Map;
+
+
+  constructor(private cdRef: ChangeDetectorRef) {}
   ngAfterViewInit(): void {
 
     if (!this.divMap) throw 'HTML element not found';
 
-    const map = new mapboxgl.Map({
+    this.map = new mapboxgl.Map({
       accessToken: environment.mapbox_key,
       container: this.divMap?.nativeElement, // container ID
       style: 'mapbox://styles/mapbox/streets-v12', // style URL
       center: [-74.5, 40], // starting position [lng, lat]
-      zoom: 9, // starting zoom
+      zoom: this.zoom, // starting zoom
     });
+
+    this.mapListeners();
+  }
+
+  mapListeners() {
+    if( !this.map) throw 'Map not initialized';
+
+    this.map.on('zoom', (ev) => {
+      this.zoom = this.map!.getZoom();
+//      this.cdRef.detectChanges();
+      this.cdRef.markForCheck();
+    });
+
+    this.map.on('zoomend', (ev) => {
+      if(this.map!.getZoom() < 18) return;
+      this.map!.zoomTo(18)
+      this.cdRef.markForCheck();
+    });
+  }
+
+  zoomIn() {
+    this.map?.zoomIn();
+  }
+  zoomOut() {
+    this.map?.zoomOut();
+  }
+
+  zoomChanged( value: string) {
+    this.zoom = Number(value);
+    this.map?.zoomTo(this.zoom);
   }
 
 }
